b4378b231bc11b09f49f004d181c11af8832286f
heliao1 <heliofms@gmail.com>
2026-06-17 21:11:12 -0300

    feat: add Windows Forms project source code

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace MinhaTelaFirebird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.Pesquisar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(231, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(964, 340);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Buscar Empresa";
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Location = new System.Drawing.Point(418, 24);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(100, 20);
+            this.txtBusca.TabIndex = 3;
+            this.txtBusca.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Pesquisar
+            // 
+            this.Pesquisar.Location = new System.Drawing.Point(165, 24);
+            this.Pesquisar.Name = "Pesquisar";
+            this.Pesquisar.Size = new System.Drawing.Size(121, 33);
+            this.Pesquisar.TabIndex = 4;
+            this.Pesquisar.Text = "Pesquisar";
+            this.Pesquisar.UseVisualStyleBackColor = true;
+            this.Pesquisar.Click += new System.EventHandler(this.Pesquisar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Busca";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleName = "";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1313, 450);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Silver;
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1305, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Empresa";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1305, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Pesquisa";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 240);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Empresa";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(393, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(315, 240);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Endereço";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(798, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 201);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Observações";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nome Fantasia";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Logradouro";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(236, 36);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Número";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(9, 78);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(246, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Complemento";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Pesquisar);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TekConsulta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.Button Pesquisar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿// Esta linha permite que o C# use os comandos do Firebird
+using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Windows.Forms;
+
+namespace MinhaTelaFirebird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1. Configure as informações do seu banco de dados local
+            // ATENÇÃO: Altere o caminho C:\SeuBanco\dados.fdb para o caminho real do seu arquivo
+            string caminhoBanco = @"C:\Users\Helio\Downloads\firebird\teknikao.fdb";
+
+            string connString = $"DataSource=localhost;Database={caminhoBanco};User ID=SYSDBA;Password=masterkey;Dialect=3;";
+
+            // 2. Escreva o comando SQL para buscar os dados
+            // ATENÇÃO: Substitua CLIENTES pelo nome em MAIÚSCULO da sua tabela do Firebird
+            string query = "SELECT * FROM EMPRESAS";
+
+            // 3. Abre a conexão e busca os dados de forma segura
+            using (FbConnection conexao = new FbConnection(connString))
+            {
+                try
+                {
+                    conexao.Open(); // Abre as portas do banco
+
+                    // Cria um adaptador para ler os dados da query SQL
+                    FbDataAdapter adapter = new FbDataAdapter(query, conexao);
+
+                    // Cria uma tabela virtual na memória do computador
+                    DataTable tabelaDados = new DataTable();
+
+                    // Preenche essa tabela virtual com os dados do Firebird
+                    adapter.Fill(tabelaDados);
+
+                    // Joga os dados da tabela virtual para o componente Grid da sua tela
+                    dataGridView1.DataSource = tabelaDados;
+
+                    MessageBox.Show("Dados carregados com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // Caso dê algum erro (caminho errado, senha errada, etc), ele avisa aqui
+                    MessageBox.Show("Erro ao conectar ou buscar dados: " + ex.Message, "Erro de Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pesquisar_Click(object sender, EventArgs e)
+        {
+          
+        {
+            // Configure as informações do seu banco de dados local (Lembre de ajustar o nome do arquivo .fdb)
+            string caminhoBanco = @"C:\Users\Helio\Downloads\firebird\teknikao.fdb";
+            string connString = $"DataSource=localhost;Database={caminhoBanco};User ID=SYSDBA;Password=masterkey;Dialect=3;";
+
+                // 1. Criamos a consulta SQL base buscando da tabela EMPRESAS
+                // Usamos 'WHERE NOME LIKE @termo' para filtrar o texto digitado
+                // ATENÇÃO: Substitua 'NOME' pelo nome real da coluna de texto da sua tabela (ex: RAZAO_SOCIAL, NOME_FANTASIA)
+                //string query = "SELECT * FROM EMPRESAS WHERE UPPER(NOME) LIKE UPPER(@termo)"; FUNCIONANDO
+                string query = "SELECT  h.data AS data,\r\n    f.nome,\r\n    h.nota, \r\n    e.nome AS empresa, \r\n    e.estado AS ES,\r\n    e.cidade, \r\n    e.cnpj\r\nFROM empresas AS e\r\nINNER JOIN historicos AS h ON e.id_empresa = h.id_empresa\r\nINNER JOIN funcionarios AS f ON h.id_funcionario = f.id_funcionario\r\nWHERE e.nome CONTAINING COALESCE(NULLIF(:busca_nome, ''), e.nome)\r\n  AND e.cnpj = COALESCE(NULLIF(:busca_cnpj, ''), e.cnpj)\r\nORDER BY h.data DESC;";
+
+                using (FbConnection conexao = new FbConnection(connString))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    // Usamos o FbCommand para injetar o texto de busca de forma segura (evita ataques de SQL Injection)
+                    using (FbCommand comando = new FbCommand(query, conexao))
+                    {
+                        // O símbolo '%' antes e depois faz com que o Firebird busque o texto em qualquer parte do nome
+                        string termoBusca = "%" + txtBusca.Text + "%";
+                        comando.Parameters.AddWithValue("@termo", termoBusca);
+
+                        // Criamos o adaptador passando o comando configurado com o parâmetro
+                        FbDataAdapter adapter = new FbDataAdapter(comando);
+                        DataTable tabelaDados = new DataTable();
+                        adapter.Fill(tabelaDados);
+
+                        // Exibe os dados filtrados na tabela da tela
+                        dataGridView1.DataSource = tabelaDados;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao buscar dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+    }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //private void button2_Click(object sender, EventArgs e)
+        {
+            // 1. Criamos uma "instância" (uma cópia) da nova tela na memória
+            Form2 Busca = new Form2();
+
+            // 2. Exibimos a nova tela
+            Busca.Show();
+        }
+
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //private void Form1_Load(object sender, EventArgs e)
+        {
+            // 1. Criamos a instância do Form2
+            Form2 telaAba = new Form2();
+
+            // 2. IMPORTANTE: Avisamos que ele NÃO é uma janela independente
+            telaAba.TopLevel = false;
+
+            // 3. Removemos as bordas de janela (botões de fechar, maximizar, etc.)
+            telaAba.FormBorderStyle = FormBorderStyle.None;
+
+            // 4. Fazemos ele preencher todo o espaço da aba escolhida
+            telaAba.Dock = DockStyle.Fill;
+
+            // 5. Adicionamos o Form2 de fato dentro dos controles da tabPage2
+            tabPage2.Controls.Add(telaAba);
+
+            // 6. Mandamos o Form2 se desenhar e aparecer dentro da aba
+            telaAba.Show();
+        }
+
+    }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
